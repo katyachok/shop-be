0c1062aac6ebd3cc3f171fc32c15e86cd65e0068
katyachok <katetestwo@outlook.com>
2021-09-09 01:43:42 +0300

    add error handler

--- product-service/src/functions/getProductsById/handler.ts
+++ product-service/src/functions/getProductsById/handler.ts
@@ -1,22 +1,25 @@
 import 'source-map-support/register';
 
 import { formatJSONResponse } from '@libs/apiGateway';
+import { errorHandler } from '@libs/errorHandler';
 import { middyfy } from '@libs/lambda';
 import { Client } from 'pg';
 import { dbOptions } from '@functions/utils.js';
 
 const getProductsById = async (event: any) => {
-    const client = new Client(dbOptions);
-    await client.connect();
+  const client = new Client(dbOptions);
+  await client.connect();
+  
   try {
+    console.log('productId request', event.pathParameters?.productId);
     const ddlResult = await client.query(`SELECT * FROM products, stocks WHERE products.id = '${event.pathParameters?.productId}' and stocks.product_id = '${event.pathParameters?.productId}'`);
-    console.log('ddlResult in productId request', ddlResult, event.pathParameters?.productId);
     const res = ddlResult.rows[0];
     return formatJSONResponse({
       res,
     });
   } catch (err) {
-    console.error(err)
+    console.error(err);
+        return errorHandler(500);
   } finally {
     client.end()
   }
--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,6 +1,7 @@
 import 'source-map-support/register';
 
 import { formatJSONResponse } from '@libs/apiGateway';
+import { errorHandler } from '@libs/errorHandler';
 import { middyfy } from '@libs/lambda';
 import { Client } from 'pg';
 import { dbOptions } from '@functions/utils.js';
@@ -12,18 +13,20 @@ const getProductsList = async (event: any) => {
   try {
     switch (event.httpMethod) {
       case 'GET': {
+        console.log('getProducts', event);
         const ddlResult = await client.query(`SELECT * FROM products RIGHT JOIN stocks ON stocks.product_id = products.id`);
-        console.log('ddlResult in getProductsList', ddlResult);
         const { rows } = ddlResult;
         return formatJSONResponse({
           rows,
         });
       }
       case 'POST': {
-        console.log('post request', event.body);
+        console.log('post create product request', event.body);
         const id = uuidv4();
         const { title, description, price, count } = event.body;
         console.log('post request event body', title, description, price, count, id);
+
+        if (!title || !price || !count) return errorHandler(400);
 
         await client.query(`insert into products (id, title, description, price) values ('${id}', '${title}', '${description}', '${price}')`);
         await client.query(`insert into stocks (product_id, count) values ('${id}', '${count}')`);
@@ -33,6 +36,7 @@ const getProductsList = async (event: any) => {
     }
   } catch (err) {
     console.error('Error during db request:', err);
+    return errorHandler(500);
   } finally {
     client.end()
   }
